afb64f0a068394602adcdfa08a8cd97a0b6d0df7
Karim Mahamed <mahamed.karim.15@gmail.com>
2017-10-07 14:14:08 +0300

    added file uploader template, html, js and servlet for web

---
+++ Sayday/WebContent/js/upload.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	
+	
+	$("#right-toggle").change(function() {
+		  $('#context1').toggle();
+		  $('#context2').toggle();
+	});
+		
+	$('#upload-submit-all').click(function(){
+		alert('clicked on upload-submit-all');
+	});
+	
+	$('#hash-submit-all').click(function(){
+		alert('clicked on hash-submit-all');
+	});
+	
+	/*Template for sending for an AJAX request on click trigger
+	 * 
+	 * 
+	 $( "#<buttonID>" ).click(function() {
+        $.ajax({
+          type: "POST",
+          url: "<Target URL here>",
+          data: <some Json data>,
+          success: function(){
+        	  alert('Request success - 200 OK');
+          },
+          error: function(){
+        	  alert('Request failure');
+          }
+        })
+    });
+    
+	 * 
+	 * */
+	
+});
