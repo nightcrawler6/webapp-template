75706ad6d7a610829d22900dd48108b5e94c11cd
Karim <Karim@10.0.0.10>
2017-11-24 03:40:44 +0200

    added ability to press 'enter' and receive send behaviour

--- Sayday/WebContent/js/so-index.js
+++ Sayday/WebContent/js/so-index.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	$("#question-holder").keyup(function(event) {
+	    if (event.keyCode === 13) {
+	        $("#submit-button").click();
+	    }
+	});
 	$('#submit-button').on('click', function(e){
 		var question = $('#question-holder').val();
 		if (question == undefined || question == ""){
@@ -8,6 +13,8 @@ $(document).ready(function(){
 		var data = {}
 		data["Action"] = "ask";
 		data["Question"] = question;
+		$('#main-header').css('z-index', 14);
+		$('#loader-div').fadeIn();
 		$.ajax({
           type: "POST",
           url: "SOServlet",
@@ -17,16 +24,35 @@ $(document).ready(function(){
         	  $('#question-holder').val('');
         	  var panelObject = $("<div class='panel panel-success'></div>");
         	  var panelHeading = $("<div class='panel-heading'></div>");
-        	  var textHeading = $("<string><h1 class='panel-title'>" + response.title + "</h1></strong><h3>" + response.question+ "</h3>");
+        	  var titleHeading = $("<h1 class='panel-title'>" + response.title + "</h1>");
+        	  var questionHeading = $("<h3>" + response.question+ "</h3>")
+        	  var XGlyph = $("<span class='glyphicon glyphicon-remove' style='float:right; cursor:pointer;'></span>");
+        	  XGlyph.on('click', function(event){
+        		 var initiator = event.target;
+        		 var holder = $(this).parent().parent().parent();
+        		 holder.fadeOut();
+        	  });
+        	  titleHeading.append(XGlyph);
         	  var panelBody = $("<div class='panel-body'>" + response.answer + "</div>")
-        	  panelHeading.append(textHeading);
+        	  panelHeading.append(titleHeading);
+        	  panelHeading.append(questionHeading);
         	  panelObject.append(panelHeading);
         	  panelObject.append(panelBody);
         	  $("#results-container").prepend(panelObject);
+        	  var body = $("html, body");
+        	  body.stop().animate({scrollTop:0}, 500, 'swing');
+        	  restoreVisibility();
           },
           error: function(){
         	  alert('Request failure');
+        	  restoreVisibility();
           }
         });
 	})
 });
+
+function restoreVisibility(){
+	$('#loader-div').fadeOut();
+	$("#question-holder").blur();
+	$('#main-header').css('z-index', 18);
+}
